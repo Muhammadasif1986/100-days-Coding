b13678df64e66d080b1962a95ffc3b482dedaadb
MUHAMMAD ASIF <162907407+Muhammadasif1986@users.noreply.github.com>
2024-05-01 05:30:26 -0700

    Day-18
    Day-18

---
+++ Day-18/q52/app.js
@@ -0,0 +1,16 @@
+// Question 52: Make a Smartphone Object: Create a simple way to keep 
+// track of a smartphone's details. Include its brand, model,
+//  and other key features like how much storage it has,
+//   the size of its screen, and how long its battery lasts.
+// Sets up details about a smartphone
+var smartphone = {
+    make: "Samsung",
+    model: "Grand prim Plus",
+    specs: {
+        storage: "8GB",
+        screenSize: "5 inches",
+        batteryLife: "8 hours"
+    }
+};
+// Shows what we've set up about the smartphone
+console.log(smartphone);
