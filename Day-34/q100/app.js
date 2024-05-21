fdc733d482333d4b647534ec2bed7d6be21b293c
MUHAMMAD ASIF <162907407+Muhammadasif1986@users.noreply.github.com>
2024-05-21 11:04:06 -0700

    Day-34
    Day-34

---
+++ Day-34/q100/app.js
@@ -0,0 +1,4 @@
+// Question 100: Use the JavaScript Math object to find the square root of 144.
+var num = 144;
+var square = Math.sqrt(num);
+console.log(square);
---
+++ Day-34/q100/app.ts
@@ -0,0 +1,5 @@
+// Question 100: Use the JavaScript Math object to find the square root of 144.
+
+let num:number = 144;
+let square:number = Math.sqrt(num);
+console.log(square);
