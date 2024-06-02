f19eb3c303929bc10c799643994591a7d3e710e2
MUHAMMAD ASIF <162907407+Muhammadasif1986@users.noreply.github.com>
2024-06-02 03:13:54 -0700

    Day-40
    Day-40

---
+++ Day-40/q120/app.ts
@@ -0,0 +1,13 @@
+// Question 120: Use a for...of loop to iterate through an array of your favorite
+//  movies and log each one.
+
+// Explain & TIP: The for...of loop is a modern loop introduced in ES6, 
+// designed to iterate over iterable objects like arrays, strings, Maps, NodeLists, 
+// and more, making it very readable and easy to use.
+let movies: string[] = ["The Shawshank Redemption", "The Godfather", "The Dark Knight", "Pulp Fiction", "Forrest Gump"];
+
+for(let movie of movies){
+      console.log(movie);
+}
+
+
